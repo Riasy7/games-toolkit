7cc6cbb41e26b35f0d7bcef4e2b0d88d0ca5d873
Ahmad <ahmad.saadawi7@gmail.com>
2026-06-16 18:36:18 -0400

    updated main.lua to include input manager changes

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 local stateManager = require("toolkit.state_manager")
+local input = require("toolkit.input_mapper")
 
 function love.load()
+	-- hardcoded for testing
+	input.load({ space = "SELECT", g = "GO_TO_SANDBOX", m = "GO_TO_MENU", escape = "QUIT" })
+
 	stateManager.add("menu", require("game.states.menu"))
 	stateManager.add("sandbox", require("game.states.sandbox"))
 
@@ -9,6 +13,10 @@ end
 
 function love.update(dt)
 	stateManager.update(dt)
+
+	if input.isPressed("QUIT") then
+		love.event.quit()
+	end
 end
 
 function love.draw()
@@ -17,11 +25,15 @@ end
 
 -- quick test
 function love.keypressed(key)
-	if key == "g" then
+	input.pressed(key)
+
+	if input.isPressed("GO_TO_SANDBOX") then
 		stateManager.switch("sandbox")
-	elseif key == "m" then
+	elseif input.isPressed("GO_TO_MENU") then
 		stateManager.switch("menu")
-	elseif key == "escape" then
-		love.event.quit()
 	end
+end
+
+function love.keyreleased(key)
+	input.released(key)
 end
