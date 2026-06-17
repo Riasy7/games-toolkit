67e18a477a6b800f00babeb0dba7ecc541df1236
Ahmad <ahmad.saadawi7@gmail.com>
2026-06-16 21:07:13 -0400

    implemented an abstract world system to handle game objects

--- toolkit/world.lua
+++ toolkit/world.lua
@@ -3,10 +3,40 @@
 local World = {}
 local objects = {}
 
+-- Given an object of any type of entity, insert it into the objects table
+-- @param object: any type of entity, player, weapon...
+--
+-- example: World.add(player) inserts player object into objects
 function World.add(object)
 	table.insert(objects, object)
 end
 
-function World.update(dt) end
+-- Given a delta time dt, any object contained in the world is updated (garbage collection as well)
+-- @param dt: delta time
+--
+-- example: World.update(dt) automatically updates player, enemies, and bullets...
+function World.update(dt)
+	for i = #objects, 1, -1 do -- count backwards to account for the shifting of the table when table.remove happens
+		local obj = objects[i]
+		if obj.is_dead then
+			table.remove(objects, i) -- potential TODO, make this O(1) instead of O(n), but for now its fine
+		else
+			obj:update(dt)
+		end
+	end
+end
 
-function World.draw() end
+-- Draws any live object in the objects table
+--
+-- example: World.draw() automatically draws all players, enemies, and bullets..
+function World.draw()
+	for i = 1, #objects do
+		objects[i]:draw()
+	end
+end
+
+-- Clears the objects table from all its values
+function World.clear()
+	objects = {}
+end
+return World
